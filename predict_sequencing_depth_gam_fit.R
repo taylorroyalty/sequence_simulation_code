43aef11bc7364f8208f31952000bba46da7da180
taylorroyalty <40607830+taylorroyalty@users.noreply.github.com>
2019-06-23 15:46:58 -0400

    Update predict_sequencing_depth_gam_fit.R

--- predict_sequencing_depth_gam_fit.R
+++ predict_sequencing_depth_gam_fit.R
@@ -1,8 +1,9 @@
+#load in fit object and predict sequencing effort (Figure 5)
 genome_size<-1e6 #in base pairs
 target_fraction<-0.5 #desired fraction of the target genome (values: 0 to 1)
 metagenome_prop<-0.5 #proportion of metagenome of the target genome (values: 0 to 1)
 
-load(file = "gam_object_Figure_5.rda")
+load(file = "GRASE_FIT_OBJECT.rda")
 
 #note probability is the legacy term for proportion of metagenome variable used above
 df_tmp<-data.frame(genome_size=genome_size,target=target_fraction,probability=metagenome_prop)
